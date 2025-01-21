befcbc8221205486c60110140ba2acd2b988b6f8
dewaley <adewaleademiluagmail.com>
2025-01-21 21:59:51 +0100

    new fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Satisfy } from "next/font/google";
+import { Satisfy } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const satisfy = Satisfy({
   variable: "--font-satisfy",
   subsets: ["latin"],
   weight: "400",
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -31,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${satisfy.variable} antialiased`}
+        className={`${satisfy.variable} antialiased`}
       >
         {children}
       </body>
