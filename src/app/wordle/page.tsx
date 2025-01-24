a46412486cf935891b3c286eb610195090ec7546
dewaley <adewaleademiluagmail.com>
2025-01-24 02:05:23 +0100

    added give up button

--- src/app/wordle/page.tsx
+++ src/app/wordle/page.tsx
@@ -321,26 +321,37 @@ const Wordle = () => {
           <p>🟨 Correct letter, wrong position</p>
           <p>⬜️ Letter not in word</p>
         </div>
-        <Button
-          className="bg-teal-800 text-white hover:bg-teal-600 my-4 mx-auto"
-          onClick={resetGame}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-6"
+        <div className="flex gap-4">
+          <Button
+            className="bg-teal-800 text-white hover:bg-teal-600 my-4 mx-auto"
+            onClick={resetGame}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-            />
-          </svg>{" "}
-          Reset word
-        </Button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+              />
+            </svg>{" "}
+            Reset word
+          </Button>
+          <Button
+            className="bg-red-800 text-white hover:bg-red-600 my-4 mx-auto"
+            onClick={() => {
+              setGameStatus("lost");
+              setIsOpen(true);
+            }}
+          >
+            Give up
+          </Button>
+        </div>
         <div>{renderKeyboard()}</div>
       </div>
 
